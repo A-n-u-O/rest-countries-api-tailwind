f18cbdb33688b2afb1d28c8ffb89111ea6128fc4
Anuoluwapo <anuvictor2005@gmail.com>
2024-03-28 21:58:09 +0100

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             <Route path='/:name' element = {<SingleCountry/>} ></Route>
           </Routes>
         </BrowserRouter>
-        <p>Made by <a href='https://www.linkedin.com/in/a-n-u-o/'>Anuoluwapo</a>&copy;2024.</p>
+        <p className=' flex items-center justify-center text-xl text-blue-600 tracking-wider'>Made by <a className=' text-red-300 tracking-wider' href='https://www.linkedin.com/in/a-n-u-o/'> Anuoluwapo </a>&copy;2024.</p>
     </>
   )
 }
