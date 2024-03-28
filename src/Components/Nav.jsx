3995b4a5e0a9fc5df608be7e035623f0cc822ff2
Anuoluwapo <anuvictor2005@gmail.com>
2024-03-28 21:50:22 +0100

    changes made

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,12 @@
-import { Header } from "./Header"
 import { Theme } from "./Theme"
-import { faMagnifyingGlass, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 
 const Nav = ({theme ,handleThemeSwitch}) => {
   return (
     <>
         <nav className="flex flex-row gap-4  items-center justify-between mb-8">
-          <span>Where in the world</span>
+          <h1 className=" font-bold text-2xl">Where in the world ?</h1>
           <Theme   theme={theme} handleThemeSwitch={handleThemeSwitch} faSun={faSun} faMoon={faMoon}  />
         </nav>
     </>
